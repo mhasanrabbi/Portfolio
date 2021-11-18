7086b54cbe1b48dc6c3cf30f4052f809bec0b8b1
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2021-11-18 22:01:22 +0600

    blog component

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -11,11 +11,10 @@ export const Blogs = ({ blogs, title, showLink }) => {
           return <Blog key={blog.id} {...blog} />
         })}
       </div>
-      {showLink && (
-        <Link to="/blog" className="btn center-btn">
-          blog
-        </Link>
-      )}
+
+      <Link to="/blog" className="btn center-btn">
+        blog
+      </Link>
     </section>
   )
 }
