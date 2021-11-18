736d3ff49f7dc33178bd21f0f7f97c927916f640
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2021-11-18 19:51:04 +0600

    projects page

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,8 +2,33 @@ import React from "react"
 import PropTypes from "prop-types"
 import Image from "gatsby-image"
 import { FaGithubSquare, FaShareSquare } from "react-icons/fa"
-const Project = () => {
-  return <h2>project copmonent</h2>
+const Project = ({ description, title, github, stack, url, image, index }) => {
+  console.log(image.localFile)
+  return (
+    <article className="project">
+      <Image
+        fluid={image[0].localFile.childImageSharp.fluid}
+        className="project-img"
+      />
+      <div className="project-info">
+        <h3>{title}</h3>
+        <p className="project-desc">{description}</p>
+        <div className="project-stack">
+          {stack.map(item => {
+            return <span key={item.id}>{item.title}</span>
+          })}
+        </div>
+        <div className="projects-links">
+          <a href={github}>
+            <FaGithubSquare className="project-icon" />
+          </a>
+          <a href={url}>
+            <FaShareSquare className="project-icon" />
+          </a>
+        </div>
+      </div>
+    </article>
+  )
 }
 
 Project.propTypes = {}
