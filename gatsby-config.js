b58ed0c7b01a3cbe1aef45bc3791f2ffe927dc4d
Mahmud Hasan <mhmdhasanrabbi@gmail.com>
2021-11-18 15:07:32 +0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
       options: {
         apiURL: `http://localhost:1337`,
         queryLimit: 1000, // Default to 100
-        //   contentTypes : `jobs`, `projects`, `blogs`,
+        //   collectionTypes : `jobs`, `projects`, `blogs`,
         //   singleType : `about`
         //  ONLY ADD TO ARRAY IF YOU HAVE DATA IN STRAPI !!!!
         contentTypes: [],
