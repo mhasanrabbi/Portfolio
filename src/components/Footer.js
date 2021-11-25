64821f73cdb7d34c82ef33fca6d4005cffc8e8db
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2021-11-25 12:24:01 +0600

    footer message

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,8 @@ const Footer = () => {
       <div>
         <SocialLinks styleClass="footer-links" />
         <h4>
-          &copy;{new Date().getFullYear()},<span>Mahmud Hasan Rabbi</span>
+          &copy;{new Date().getFullYear()},<span>Mahmud Hasan Rabbi</span>{" "}
+          running with github actions
         </h4>
       </div>
     </footer>
